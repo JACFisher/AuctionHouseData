2348143fd736696ecf4b2c55875f89c12d7434ca
JACFisher <jfishe04@rams.shepherd.edu>
2020-10-30 13:50:22 -0400

    added logging to file

--- data_scraper.py
+++ data_scraper.py
@@ -6,12 +6,15 @@
 # alternate ids  :  74 96 156 1068 1259 1267 1276 1280 1567
 #
 #
-# TODO: error logging
 # TODO: do not search for the name each time!  do it ONCE per item, maybe later in process?
 
 import datetime
 import json
 import requests
+import logging
+# the following are only used in main :|
+import os.path
+import sys
 
 # global variables:
 
@@ -43,16 +46,14 @@ url_pieces = \
             }
     }
 error_codes = [404]  # expand this list
+log_filename = "data_retriever.log"
+request_warning = "During {}, the following status code was received: {}"
 
 
-class DataScraper(object):
+class DataRetriever(object):
 
-    def __init__(self, config_file="config.json", filename=None):
-        if filename is None:
-            cur_time = datetime.datetime.now().timestamp()
-            self.filename = str(cur_time) + ".json"
-        else:
-            self.filename = filename
+    def __init__(self, config_file="config.json", source="program process"):
+        initialize_logger(source)
         # the following block is handled with load_config:
         self.region = None
         self.token_data = None
@@ -63,49 +64,68 @@ class DataScraper(object):
         self.token = None
 
     def load_config(self, config):
-        with open(config, 'r') as openfile:
-            config_json = json.load(openfile)
-            openfile.close()
-        self.token_data = config_json["token_data"]
-        self.realm_id_list = config_json["realm_id"]
-        if type(self.realm_id_list) is not list:
-            self.realm_id_list = [self.realm_id_list]  # big brain
-        self.region = config_json["region"]
-        self.locale = config_json["locale"]
+        if os.path.isfile(config):
+            with open(config, 'r') as openfile:
+                config_json = json.load(openfile)
+                openfile.close()
+            self.token_data = config_json["token_data"]
+            self.realm_id_list = config_json["realm_id"]
+            if type(self.realm_id_list) is not list:
+                self.realm_id_list = [self.realm_id_list]  # big brain
+            self.region = config_json["region"]
+            self.locale = config_json["locale"]
+        else:
+            logging.ERROR("Config file not found: " + config)
 
     def fetch_token(self):
         url = token_url_by_region[self.region]
         token_post = requests.post(url, self.token_data)
         token_json = json.loads(token_post.text)
         self.token = token_json["access_token"]
+        logging.info("Token received: " + self.token)
 
     def fetch_ah_data(self) -> dict:
-        # try each realm id until we get one that doesn't error
+        # try each realm id in list of connected realms until we get one that doesn't error
         this_attempt = None
+        logging.info("Reaching out to auction house api at: " + get_timestamp(human_readable=True))
         for realm_id in self.realm_id_list:
             ah_url = self.build_url(url_type="auction", data=realm_id)
             this_attempt = requests.get(ah_url)
+            logging.info("Attempted call to: " + ah_url)
             if this_attempt.status_code not in error_codes:
+                # We hit a good one
+                logging.info("Realm ID " + str(realm_id) + " returned a valid code: " + str(this_attempt.status_code))
                 break
+            else:
+                logging.info("Error code received: " + str(this_attempt.status_code))
         if this_attempt.status_code in error_codes:
-            exit(1)  # handle error logging instead
+            logging.warning(request_warning.format("auction data retrieval", str(this_attempt.status_code)))
+            return {} # return an empty dict if we never accepted data other than error codes
         else:
             return json.loads(this_attempt.text)
 
-    def merge_item_names(self, cleaned_data):
+    def merge_item_names(self, data):
         merged_data = {}
-        for item_id in cleaned_data.keys():
-            item_url = self.build_url(url_type="item", data=item_id)
-            this_item = requests.get(item_url)
-            if this_item.status_code not in error_codes:
-                item_data = json.loads(this_item.text)
-                item_name = item_data["name"]
-            else:
-                item_name = "UNKNOWN"
-            auction_data = cleaned_data.get(item_id)
+        for item_id in data.keys():
+            item_name = self.fetch_item_name(item_id)
+            auction_data = data.get(item_id)
             auction_data.update({"name": item_name})
             merged_data.update({item_id: auction_data})
         return merged_data
+
+    def fetch_item_name(self, item_id):
+        item_url = self.build_url(url_type="item", data=item_id)
+        logging.info("Reaching out to item api at: " + get_timestamp(human_readable=True))
+        this_item = requests.get(item_url)
+        logging.info("Attempted call to: " + item_url)
+        if this_item.status_code not in error_codes:
+            item_data = json.loads(this_item.text)
+            item_name = item_data["name"]
+            logging.info("Name collected: " + item_name)
+            return item_name
+        else:
+            logging.WARNING("Received error code: " + str(this_item.status_code))
+            return "UNKNOWN"
 
     def build_url(self, url_type, data) -> str:
         url = api_url_by_region[self.region]
@@ -119,18 +139,31 @@ class DataScraper(object):
         url += self.token
         return url
 
-    def main(self):
-        self.fetch_token()
-        auction_data = self.fetch_ah_data()
-        cleaned_data = clean_auction_data(auction_data)
-        final_data = self.merge_item_names(cleaned_data)
-        with open(self.filename, 'w') as wf:
-            json.dump(final_data, wf, indent=4, sort_keys=True)
+    def gather_data(self) -> dict:
+        if self.token_data is None:
+            logging.WARNING("Attempted to gather data without a token at: " + get_timestamp(human_readable=True))
+            return {}
+        else:
+            self.fetch_token()
+            auction_data = self.fetch_ah_data()
+            clean_data = clean_auction_data(auction_data)
+            return clean_data
+
+    # This method is very slow - each name requires a get request!
+    # Recommended not to use this, rather gather data ONCE
+    def merge_item_names(self, data):
+        merged_data = {}
+        for item_id in data.keys():
+            item_name = self.fetch_item_name(item_id)
+            auction_data = data.get(item_id)
+            auction_data.update({"name": item_name})
+            merged_data.update({item_id: auction_data})
+        return merged_data
 
 
 def clean_auction_data(auction_data) -> dict:
     auction_list = auction_data["auctions"]
-    cleaned_data = {}
+    clean_data = {}
     for auction in auction_list:
         item_id = auction["item"]["id"]
         possible_keys = ["quantity", "buyout", "unit_price"]
@@ -140,10 +173,47 @@ def clean_auction_data(auction_data) -> dict:
                 temp_dict.update({key: auction[key]})
             else:
                 temp_dict.update({key: "NONE"})
-        cleaned_data.update({item_id: temp_dict})
-    return cleaned_data
+        clean_data.update({item_id: temp_dict})
+    return clean_data
+
+
+def initialize_logger(source):
+    logging.basicConfig(filename=log_filename, encoding='utf-8', level=logging.DEBUG)
+    time = get_timestamp(human_readable=True)
+    logging.info('**************************************************')
+    logging.info("Process started at: " + time)
+    logging.info("Process called by: " + source)
+
+
+def get_timestamp(human_readable=False) -> str:
+    cur_time = datetime.datetime.now().timestamp()
+    if human_readable:
+        human_readable = datetime.datetime.fromtimestamp(cur_time).isoformat()
+        return human_readable
+    else:
+        return str(cur_time)
 
 
 if __name__ == "__main__":
-    ds = DataScraper("my_config.json")
-    ds.main()
+    # In general, should not be called directly, main method exists only to demonstrate use
+    # accepts the filename as the first command line arg or defaults to config.json
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = "my_config.json"
+    if os.path.isfile(filename):
+        dr = DataRetriever(config_file=filename, source="main method")
+        cleaned_data = dr.gather_data()
+        # Reinsert the following line to add item names to each id
+        # cleaned_data = self.merge_item_names(cleaned_data)
+        # Be aware, in my testing the process went from under a minute to over an hour
+        # The final process will do this in a more efficient way
+        data_outfile = "sample." + get_timestamp() + ".json"
+        with open(data_outfile, 'w') as wf:
+            json.dump(cleaned_data, wf, indent=4, sort_keys=True)
+            wf.close()
+    else:
+        initialize_logger(source="main method")
+        logging.error("Config file not found: " + filename)
+    logging.info("Process ended at: " + get_timestamp(human_readable=True))
+
