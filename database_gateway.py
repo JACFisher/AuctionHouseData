2001af8a27f9ba6fa12c14e895d1eafc60d2e240
JACFisher <jfishe04@rams.shepherd.edu>
2020-11-10 00:53:46 -0500

    Now builds tables, executes statements, checks name table for existing entries

--- database_gateway.py
+++ database_gateway.py
@@ -12,16 +12,18 @@ _db_name = "ah_database.db"
 _log_filename: str = "db_gateway.log"
 _tables: dict = {
         "sales_table": {
-            "name": "daily_listings",
-            "columns": ["[item_id] INTEGER PRIMARY KEY", "[year] INTEGER NOT NULL", "[day_in_year] INTEGER NOT NULL"]
+            "name": "listings",
+            "columns": ["item_id INTEGER PRIMARY KEY", "year INTEGER NOT NULL", "day_in_year INTEGER NOT NULL"],
+            "constraints": []
         },
-        "table_names": {
+        "names_table": {
             "name": "item_names",
-            "columns": ["[ite8m_id] INTEGER FOREIGN KEY", "[item_name] TEXT DEFAULT 'UNDEFINED'"]
+            "columns": ["item_id INTEGER", "item_name TEXT DEFAULT 'UNDEFINED'"],
+            "constraints": ["CONSTRAINT item_id FOREIGN KEY (item_id) REFERENCES listings (item_id)"]
         }
 }
 for x in range(0,24):
-    _tables["sales_table"]["columns"].append("[sales_at_hour_{}] INTEGER DEFAULT -1".format(x))
+    _tables["sales_table"]["columns"].append("sales_at_hour_{} INTEGER DEFAULT -1".format(x))
 
 
 class DatabaseGateway(object):
@@ -36,8 +38,8 @@ class DatabaseGateway(object):
         if self.conn is None or self.cursor is None:
             self.log.WARNING("Attempted to add data without a connection at: " + get_timestamp(human_readable=True))
         else:
-            for keys in data.keys():
-                pass
+            for key in data.keys():
+                self.check_item_table(key)
 
     def connect_to_db(self):
         try:
@@ -71,10 +73,52 @@ class DatabaseGateway(object):
             self.log.warning("Attempted to create tables without a connection at: " +
                              get_timestamp(human_readable=True))
         else:
-            create = "CREATE TABLE IF NOT EXISTS"
+            self.log.info("Creating tables if they don't exist.")
+            create = "CREATE TABLE IF NOT EXISTS {}";
+            for table_data in _tables.keys():
+                statement = create.format(_tables[table_data]["name"])
+                count = 0;
+                for column in _tables[table_data]["columns"]:
+                    if count == 0:  # need the first column to have an open parenthesis
+                        statement += " ("
+                        count += 1
+                    else:  # the rest of the columns are comma-separated
+                        statement += ", "
+                    statement += column
+                for constraint in _tables[table_data]["constraints"]:
+                    statement += ", " + constraint
+                statement += ")"
+                self.execute_statement(statement)
+
+    def execute_statement(self, statement) -> bool:
+        try:
+            self.log.info("Executing statement: ")
+            self.log.info(statement)
+            self.cursor.execute(statement)
+            self.conn.commit()
+            return True
+        except Exception as e:
+            self.log.error("Statement failed due to: ")
+            self.log.error(e)
+            return False
+
+    def check_item_table(self, item_id: int):
+        if self.conn is None or self.cursor is None:
+            self.log.warning("Attempted to access item_names without a connection at: " +
+                             get_timestamp(human_readable=True))
+        else:
+            statement = "SELECT * FROM {} WHERE item_id={}".format(
+                _tables["names_table"]["name"], str(item_id))
+            if self.execute_statement(statement):
+                if len(self.cursor.fetchall()) < 1:
+                    statement = "INSERT INTO {} (item_id) VALUES ({})".format(
+                        _tables["names_table"]["name"], str(item_id))
+                    self.execute_statement(statement)
 
 
 if __name__ == '__main__':
     dg = DatabaseGateway()
     dg.connect_to_db()
+    dg.create_tables()
+    dg.check_item_table(35)
     dg.close_connection()
